31d4d2b2c9e48e7e7847e5c167b6fbdf08b6674a
coolyang <504575307@qq.com>
2020-04-09 21:46:56 +0800

    update prettier config

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 module.exports = {
   configs: {
     recommended: {
-      extends: ['eslint:recommended', 'plugin:prettier/recommended']
+      extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            singleQuote: true,
+          },
+        ],
+      },
     },
     vue: {
-      extends: ['plugin:vue/recommended', 'prettier/vue']
+      extends: ['plugin:vue/recommended', 'prettier/vue'],
     },
     react: {
-      extends: ['plugin:react/recommended', 'prettier/react']
+      extends: ['plugin:react/recommended', 'prettier/react'],
     },
     typescript: {
       parser: '@typescript-eslint/parser',
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint'
-      ]
-    }
-  }
+        'prettier/@typescript-eslint',
+      ],
+    },
+  },
 };
