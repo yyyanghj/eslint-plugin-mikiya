7c0b13f8f3f2f6ae49ded4602aeac62c908c3a9e
coolyang <504575307@qq.com>
2020-04-12 21:45:45 +0800

    fix: vue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   configs: {
     recommended: {
       extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+      plugins: ['prettier'],
       rules: {
         'prettier/prettier': [
           'error',
@@ -13,24 +14,27 @@ module.exports = {
             printWidth: 100,
             trailingComma: 'all',
             endOfLine: 'auto',
-            bracketSpacing: true,
-          },
-        ],
-      },
+            bracketSpacing: true
+          }
+        ]
+      }
     },
     vue: {
       extends: ['plugin:vue/strongly-recommended', 'prettier/vue'],
+      plugins: ['vue']
     },
     react: {
       extends: ['plugin:react/recommended', 'prettier/react'],
+      plugins: ['react']
     },
     typescript: {
       parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-      ],
-    },
-  },
+        'prettier/@typescript-eslint'
+      ]
+    }
+  }
 };
