e89fe303a771b3eecd31d1ac8f22574f7b891ef8
yang <xelfyang@qq.com>
2021-03-21 19:43:58 +0800

    feat: vue recommended

--- examples/vue-ts/index.vue
+++ examples/vue-ts/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class=""></div>
-</template>
+  <header class="header relative flex justify-between items-center h-24 py-0 px-4">
+    <router-link class="logo" to="/">
+      <Logo />
+    </router-link>
+    <Icon name="menu" class="menu text-2xl cursor-pointer" @click="show = true" />
 
+    <Menu :show="show" @close="show = false" />
+  </header>
+</template>
 <script lang="ts">
 export default {
   name: 'Hello',
